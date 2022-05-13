7ab965fbb5942feed9711591274cf42cf4ccadee
anger <sangerer4@tsn.at>
2022-05-13 08:59:19 +0200

    Zeitabfrageprogramm Angerer

--- hello world.py
+++ hello world.py
@@ -2,8 +2,10 @@
 #Datum:13.05.2022
 #Simply Python Programm Hi World!
 
-def print_hi(name):
-    print(f'Hi, {name}')
-
-if __name__=='__main__':
-    print_hi('Patrick und Simon')
+import time
+s=time.time()
+n=input("Namen eingeben:")
+print(n)
+e=time.time()
+dif=round(e-s,1)
+print("Zeit",dif,"sek")
